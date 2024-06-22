9e65decc190efe7c1f9be4291c4223e943f57550
JustADeve <142085654+JustADeve@users.noreply.github.com>
2024-06-22 11:10:06 +0200

    Update create.js
    add tos before create vps

--- commands/create.js
+++ commands/create.js
@@ -1,6 +1,5 @@
-const { SlashCommand } = require('slashctrl');
+const { SlashCommand, MessageActionRow, MessageButton } = require('slashctrl');
 const lib = require('../lib');
-
 
 var randomip = require('random-ip');
 var generator = require('generate-password');
@@ -9,161 +8,188 @@ class CMD extends SlashCommand {
 
     constructor() {
         super();
-        
-        // this.guilds = ["1211544398219976724"];
-        
+
         this.setName("create");
-        this.setDescription("Create a vps");
+        this.setDescription("Create a VPS");
 
         this.addStringOption(option =>
             option.setName('name')
                 .setDescription('VPS name')
-				.setRequired(true));
-        
+                .setRequired(true));
 
         this.requiresAdmin = false;
     }
-    
+
     async execute(interaction) {
         if (await lib.checkAdmin(this, interaction)) return;
 
         var user = await lib.getUser(interaction);
-        
+
         var name = interaction.options.getString('name');
-        // var type = interaction.options.getString('type');
-	    var type = 'normal';
 
-	while (String(name).includes('@')) {
-		name = String(name).replace('@', '');
-	}
-
-        if (type != 'normal' && type != 'test') return await lib.error(interaction, 'Invalid vps type');
-
-        const db = require('../db');
-        var VPS = await db.VPS.find({
-            userID: interaction.user.id
-        });
-        if (VPS.length >= user.vpsLimit) {
-		if (user.vpsLimit == 0 && user.isBanned == false) {
-			 return lib.error(interaction, `You currently can't create any vps. In order to be able to create one, you will need to request one via the form: https://forms.gle/x1urbCtEHTbbRXZo9. After filling in the from, create a ticket.`);
-		} else if (user.vpsLimit == 0 && user.isBanned == true) {
-			return lib.error(interaction, `User is banned from service: ${user.banReason}`);
-		} else {
-			return lib.error(interaction, `You have reached your vps limit. You are limited to ${user.vpsLimit} vps, but you currently have ${VPS.length} vps.`);
-		}
+        while (String(name).includes('@')) {
+            name = String(name).replace('@', '');
         }
 
-	 /*   var nC;
-	if (type == 'test') {
-		nC = 'de-f1';
-	} else {
-		nC = 'ro-f1'
-	} */
-	    
-        var node = await db.Node.findOne({
-            isFull: false,
-            isAvailable: true,
- 		// code: nC
-        }).sort({ percent: 1 }).exec();
+        const rules = `
+**Rules:**
+- We have the right to delete your server at any moment without reason or notifying you.
+- Do NOT host/do the following:
+  - Bitcoin mining
+  - (bit)torrent client or server
+  - Use a high amount of CPU
+  - Illegal things
+  - DDoS'ing
+  - Bots (excluding Discord bots)
+  - Tor exit node
+  - Minecraft servers
+  - VPN
+  - Porn
+  - Video hosting and/or encoding
+  - Proxy(s)
+  - Gameservers
+  - Desktops
+  - Botnet(s)
+  - Pterodactyl
+  - Wings
+  - Traffic monetizers
+  - Any other gamepanel
+  - Pufferpanel
+  - Hacking
+  - Using high bandwidth
+Hosting illegal things will result in a ban of the service.
+Hosting anything against the law in Germany or the Netherlands will result in a ban.
 
-        if (!node) return await lib.error(interaction, 'No node available.');
+**Please note:**
+- Inactive servers will get deleted after 7 days.
+- Our service is free, so we do not have any uptime guarantee.
+`;
 
-        // console.log(node);
+        const row = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setCustomId('accept')
+                    .setLabel('Accept')
+                    .setStyle('SUCCESS'),
+                new MessageButton()
+                    .setCustomId('decline')
+                    .setLabel('Decline')
+                    .setStyle('DANGER')
+            );
 
-        await interaction.deferReply();
+        await interaction.reply({ content: rules, components: [row], ephemeral: true });
 
-        // console.log(interaction.client);
-        var queue = interaction.client.createQueue[node.code];
+        const filter = i => i.user.id === interaction.user.id;
 
-        if (!queue) return await lib.error(interaction, 'Node not found?', true);
+        const collector = interaction.channel.createMessageComponentCollector({ filter, time: 60000 });
 
-        var password = generator.generate({
-            length: 15,
-            uppercase: false,
-            numbers: true
+        collector.on('collect', async i => {
+            if (i.customId === 'accept') {
+                collector.stop('accepted');
+                await i.update({ content: 'You accepted the rules. Creating your VPS...', components: [] });
+
+                // Proceed with VPS creation
+                var type = 'normal';
+
+                if (type != 'normal' && type != 'test') return await lib.error(interaction, 'Invalid vps type');
+
+                const db = require('../db');
+                var VPS = await db.VPS.find({
+                    userID: interaction.user.id
+                });
+                if (VPS.length >= user.vpsLimit) {
+                    if (user.vpsLimit == 0 && user.isBanned == false) {
+                        return lib.error(interaction, `You currently can't create any vps. In order to be able to create one, you will need to request one via the form: https://forms.gle/x1urbCtEHTbbRXZo9. After filling in the from, create a ticket.`);
+                    } else if (user.vpsLimit == 0 && user.isBanned == true) {
+                        return lib.error(interaction, `User is banned from service: ${user.banReason}`);
+                    } else {
+                        return lib.error(interaction, `You have reached your vps limit. You are limited to ${user.vpsLimit} vps, but you currently have ${VPS.length} vps.`);
+                    }
+                }
+
+                var node = await db.Node.findOne({
+                    isFull: false,
+                    isAvailable: true,
+                }).sort({ percent: 1 }).exec();
+
+                if (!node) return await lib.error(interaction, 'No node available.');
+
+                await interaction.deferReply();
+
+                var queue = interaction.client.createQueue[node.code];
+
+                if (!queue) return await lib.error(interaction, 'Node not found?', true);
+
+                var password = generator.generate({
+                    length: 15,
+                    uppercase: false,
+                    numbers: true
+                });
+                var ip = randomip(node.subnet, node.subnetMask);
+
+                var sshPort = await db.Port.findOne({
+                    node: node.code,
+                    isUsed: false
+                });
+                if (!sshPort) return await lib.error(interaction, 'No ports available. Please contact an administrator.', true);
+                sshPort.isUsed = true;
+                sshPort.intPort = 22;
+                await sshPort.save();
+
+                await interaction.editReply('Adding to queue...');
+
+                var VPS = new db.VPS({
+                    userID: interaction.user.id,
+                    password,
+                    name,
+                    ip,
+                    sshPort: sshPort.port,
+                    sshPortID: sshPort._id,
+                    state: 'queued',
+                    isCreated: false,
+                    cost: (1/730/60),
+                    portLimit: user.portLimit,
+                    node: node.code,
+                    nodeIP: node.ip,
+                    shortID: Math.floor(1000 + Math.random() * 9000),
+                    type
+                });
+                await VPS.save();
+
+                sshPort.vpsID = VPS._id;
+                await sshPort.save();
+
+                var job = await queue.add(`vps_${interaction.user.id}-${Date.now()}`, {
+                    password,
+                    ip: VPS.ip,
+                    subnetMask: node.subnetMask,
+                    sshPort: sshPort.port,
+                    userID: interaction.user.id,
+                    nodeIP: node.ip,
+                    vpsID: VPS._id,
+                    node: node.code,
+                    portID: sshPort._id,
+                    shortID: VPS.shortID,
+                    storage: node.storage,
+                    type,
+                    subnet: node.subnet
+                });
+                VPS.jobID = job.id;
+                await VPS.save();
+
+                await interaction.editReply(`**QUEUED**\nYour vps has been placed in the queue with queue ID ${job.id} and VPS ID ${VPS.shortID} on node \`${node.code}\``);
+            } else if (i.customId === 'decline') {
+                collector.stop('declined');
+                await i.update({ content: 'You declined the rules. VPS creation aborted.', components: [] });
+            }
         });
-        var ip = randomip(node.subnet, node.subnetMask);
 
-        var sshPort = await db.Port.findOne({
-            node: node.code,
-            isUsed: false
+        collector.on('end', (collected, reason) => {
+            if (reason !== 'accepted' && reason !== 'declined') {
+                interaction.editReply({ content: 'No response received. VPS creation aborted.', components: [] });
+            }
         });
-        if (!sshPort) return await lib.error(interaction, 'No ports available. Please contact an administrator.', true);
-        sshPort.isUsed = true;
-        sshPort.intPort = 22;
-        await sshPort.save();
-
-        await interaction.editReply('Adding to queue...');
-
-        var VPS = new db.VPS({
-            userID: interaction.user.id,
-            password,
-            name,
-            ip,
-            sshPort: sshPort.port,
-            sshPortID: sshPort._id,
-            state: 'queued',
-            isCreated: false,
-            cost: (1/730/60),
-            portLimit: user.portLimit,
-            node: node.code,
-            nodeIP: node.ip,
-            shortID: Math.floor(1000 + Math.random() * 9000),
-            type
-        });
-        await VPS.save();
-
-        console.log(`${VPS.shortID} has ${ip}`);
-
-        sshPort.vpsID = VPS._id;
-        await sshPort.save();
-
-        var job = await queue.add(`vps_${interaction.user.id}-${Date.now()}`, {
-            password,
-            ip: VPS.ip,
-            subnetMask: node.subnetMask,
-            sshPort: sshPort.port,
-            userID: interaction.user.id,
-            nodeIP: node.ip,
-            vpsID: VPS._id,
-            node: node.code,
-            portID: sshPort._id,
-            shortID: VPS.shortID,
-            storage: node.storage,
-            type,
-	    subnet: node.subnet
-        });
-        VPS.jobID = job.id;
-        await VPS.save();
-
-        interaction.editReply(`**QUEUED**\nYour vps has been placed in the queue with queue ID ${job.id} and VPS ID ${VPS.shortID} on node \`${node.code}\``);
-
-	var server;
-	var inv;
-	if (interaction.guild) {
-		var guild = interaction.guild;
-		server = interaction.guild.name;
-		// var ch = await guild.channels.fetch();
-		// var ch = await guild.channels.filter(c => c.type === 'text').find(x => x.position == 0);;
-		// console.log(ch.entries().next().value);
-		// console.log(ch[1]);
-        console.log(guild.systemChannelId);
-        if (guild.systemChannelId) {
-            inv = await guild.invites.create(guild.systemChannelId, {
-                maxAge: 0,
-                reason: 'audit log',
-                unique: false
-            });
-        } else {
-            inv = 'no';
-        }
-	} else {
-		server = 'DM';
-        inv = '-';
-	}
-	interaction.log.send(`<@554344892827172884> **${interaction.user.displayName}** in ${server} (${inv}) created a vps: ${name}`);
     }
-
 }
 
 module.exports = { default: CMD };
